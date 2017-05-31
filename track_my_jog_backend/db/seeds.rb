0009f334d81a2e7b84d6a66a0588cd9d2d821a86
Santiago Vanegas <savanegasg@gmail.com>
2017-05-31 11:41:00 -0500

    Creating seeds for back end

--- track_my_jog_backend/db/seeds.rb
+++ track_my_jog_backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = FactoryGirl.create(:admin_user, email: 'admin@example.com', password: 'password')
+
+3.times do
+  user = FactoryGirl.create(:manager_user, password: 'password')
+  Faker::Number.between(0, 5).times do
+    FactoryGirl.create(:time_entry, user: user)
+  end
+end
+
+5.times do
+  user = FactoryGirl.create(:user, password: 'password')
+  Faker::Number.between(0, 5).times do
+    FactoryGirl.create(:time_entry, user: user)
+  end
+end
